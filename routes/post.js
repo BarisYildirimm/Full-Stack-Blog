0c7cbed2df864eae1bc65d76bd166048ac267ae6
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-09-01 21:28:14 +0200

    create getPost API route

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyUser.js";
-import { create } from "../controllers/post.js";
+import { create, getposts } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-// router.get("/getposts", getposts);
+router.get("/getposts", getposts);
 // router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
 // router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
