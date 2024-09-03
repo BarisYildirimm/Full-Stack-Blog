69703cba825e8e43e47e98fce2ae6663ce90e62c
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-09-03 17:02:01 +0200

    create get users API

--- routes/user.js
+++ routes/user.js
@@ -7,4 +7,5 @@ const router = express.Router();
 router.get("/", getUsers);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
+router.get("/getUsers", verifyToken, getUsers);
 export default router;
