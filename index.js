a65b90b753c3d03c1ca3e5a3d0000591c463a7f0
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-12-14 15:25:32 +0100

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 
 import errorHandler from "./middleware/errorHandler.js";
 
@@ -16,37 +15,18 @@ import commentRoutes from "./routes/comment.js";
 dotenv.config();
 const app = express();
 
-// Cookie parser middleware
-app.use(cookieParser());
-
-// Route to set the cookie
-app.get("/set-cookie", (req, res) => {
-  res.cookie("__vercel_live_token", process.env.JWT_SECRET, {
-    httpOnly: true, // JavaScript ile erişimi kapatır, güvenlik için önemli
-    secure: true, // HTTPS üzerinde çalışması için gerekli (Production ortamında)
-    sameSite: "none", // Cross-site erişime izin verir
-  });
-
-  res.status(200).send("Cookie has been set!");
-});
-
-// Route to check cookies
-app.get("/check-cookie", (req, res) => {
-  const token = req.cookies["__vercel_live_token"];
-  if (token) {
-    res.status(200).send(`Token received: ${token}`);
-  } else {
-    res.status(400).send("No token found");
-  }
-});
+// CORS Middleware'i ekleyin
+app.use(
+  cors({
+    origin:
+      "https://eunike-client-f7padbbkq-barisyildirimms-projects.vercel.app/", // Frontend domaini
+    credentials: true, // Çerez gönderimine izin verir
+  })
+);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
-app.use("/", req, (res) => {
-  return res.send("server running...");
-});
 
 app.use("/test", testRoutes);
 app.use("/api/user", userRoutes);
