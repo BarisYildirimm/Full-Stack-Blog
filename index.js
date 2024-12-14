f9a12a093369f955741e3e99fe7cfe2b4c5465e5
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-12-14 16:23:30 +0100

    update

--- index.js
+++ index.js
@@ -21,53 +21,28 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 import cors from "cors";
 import express from "express";
 
-// Üretim ortamındaki ve geliştirme ortamındaki origin listelerini ayarlayalım
-const devOrigins = ["http://localhost:5000"]; // Geliştirme ortamındaki izin verilen domain
-const prodOrigins = ["https://eunike-client.vercel.app"]; // Üretim ortamındaki izin verilen domain
-
-// Orgin ve ortamı kontrol ederek doğru listeyi seçiyoruz
+const prodOrigins = ["https://eunike-backend.vercel.app"];
+const devOrigin = ["http://localhost:5173"];
 const allowedOrigins =
-  process.env.NODE_ENV === "production" ? prodOrigins : devOrigins;
-
-// CORS Middleware'i ekleyin
+  getEnvironmentVariable("NODE_ENV") === "production" ? prodOrigins : devOrigin;
 app.use(
   cors({
     origin: (origin, callback) => {
-      // Eğer origin yoksa (yani CORS denetimi gerektirmeyen iç isteklerse), geçişe izin ver
-      if (!origin) {
-        callback(null, true);
-        return;
-      }
-
-      // Eğer origin izin verilenler arasında varsa, istek kabul edilir
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
+      if (getEnvironmentVariable("NODE_ENV") === "production") {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error(`${origin} not allowed by cors`));
+        }
       } else {
-        // Eğer origin izin verilen listede yoksa, hata veririz
-        callback(new Error(`${origin} is not allowed by CORS`));
+        callback(null, true);
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Desteklenen HTTP metodları
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"], // İzin verilen başlıklar
-    credentials: true, // Çerezlerin paylaşılmasına izin verir
-    preflightContinue: false, // Preflight isteklerinin sonrasında herhangi bir işlem yapılmaz
-    optionsSuccessStatus: 204, // Preflight için başarı durumu
+    optionsSuccessStatus: 200,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
-// Örnek bir GET endpoint
-app.get("/test", (req, res) => {
-  res.json({ message: "CORS çalışıyor!" });
-});
-
-// Hata middleware'i
-app.use((err, req, res, next) => {
-  if (err) {
-    res.status(403).json({ error: err.message }); // CORS hatası ile ilgili yanıt
-  } else {
-    next();
-  }
-});
 
 app.use("/test", testRoutes);
 app.use("/api/user", userRoutes);
