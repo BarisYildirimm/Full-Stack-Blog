29e29457e56d4d4afb7415a63acad5e8e4d5cf88
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-12-14 15:10:25 +0100

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Vercel'de kök klasör için doğru yapı
+  build: {
+    outDir: "dist", // Build çıktılarının dizini
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "https://full-stack-blog-5yxj1g3fe-barisyildirimms-projects.vercel.app/?vercelToolbarCode=qgL07BSWegcp1MK", // Backend adresi
+        changeOrigin: true, // Cross-site sorunlarını çözer
+        secure: true, // HTTPS kullanıyorsanız true
+      },
+    },
+  },
 });
