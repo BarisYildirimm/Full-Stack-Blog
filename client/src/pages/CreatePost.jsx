e70ce479c73cb9f86b58e663797585c4989f68e4
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-12-14 15:00:34 +0100

    update 3

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -45,7 +45,7 @@ export default function CreatePost() {
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           setImageUploadProgress(progress.toFixed(0));
         },
-        () => {
+        (error) => {
           setImageUploadError("Image upload failed");
           setImageUploadProgress(null);
         },
@@ -84,7 +84,7 @@ export default function CreatePost() {
         setPublishError(null);
         navigate(`/post/${data.slug}`);
       }
-    } catch () {
+    } catch (error) {
       setPublishError("Something went wrong");
     }
   };
