ca496ad5cbd5042945b949d851562580894194e7
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-12-14 15:19:03 +0100

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import errorHandler from "./middleware/errorHandler.js";
 
