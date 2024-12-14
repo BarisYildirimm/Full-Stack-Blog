a73c383c9f1d2fd8d82255008f7c9268793f3619
BarisYildirim00 <barisyildirim1023@outlook.com>
2024-12-14 15:53:55 +0100

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Vercel'de kök klasör için doğru yapı
+  base: "/", // Kök klasör ayarı
   build: {
-    outDir: "dist", // Build çıktılarının dizini
+    outDir: "dist", // Frontend çıktılarının dizini
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://eunike-backend.vercel.app", // Backend API sunucusu
+        changeOrigin: true,
+        secure: false, // HTTPS kullanmıyorsanız false yapın
+      },
+    },
   },
 });
